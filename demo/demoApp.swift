776777735a8e3734770cc75ab7cfd98f577ae5f1
yoloyash <ykhurana6@gmail.com>
2024-08-03 17:17:08 -0500

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Yash Khurana on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
